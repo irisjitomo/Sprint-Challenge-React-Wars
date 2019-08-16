eb6a87aac3a273483870326fa27752d7f7d98a9a
irisjitomo <irisjitomo@eyahoo.com>
2019-08-16 13:21:37 -0400

    completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import StarWarsParent from './components/StarWarsParent';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const Header = styled.header`
+    height:200px;
+  `;
+
+  const H1 = styled.h1` 
+  font-size:5rem;
+  color: #feda4a;
+  text-shadow: 4px 4px black, 4px 4px black, 4px 4px black, 4px 4px black;
+    padding: 40px;
+  `;
+  
+
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Header>
+      <H1 className="Header">React Wars</H1>
+      </Header>
+      <StarWarsParent/>
+      <footer></footer>
     </div>
   );
 }
